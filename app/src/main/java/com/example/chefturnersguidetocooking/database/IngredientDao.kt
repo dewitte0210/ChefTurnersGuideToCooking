211fb10494181ade698e1ab95ef915e7e14de007
kruegerj9819 <144365600+kruegerj9819@users.noreply.github.com>
2024-04-07 18:07:50 -0500

    Ingredient Query Test

--- app/src/main/java/com/example/chefturnersguidetocooking/database/IngredientDao.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/database/IngredientDao.kt
@@ -1,4 +1,11 @@
 package com.example.chefturnersguidetocooking.database
 
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface IngredientDao {
+    @Query("SELECT * FROM Ingredient")
+    fun getAllIngredients(): Flow<List<Ingredient>>
 }
