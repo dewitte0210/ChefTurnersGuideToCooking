1b8e31c04822f63befe6ee97e31bc126d4b8fd30
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-04-07 18:23:47 -0500

    Recipe and Recipe Ingredient Dao made

--- app/src/main/java/com/example/chefturnersguidetocooking/database/RecipeIngredientDao.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/database/RecipeIngredientDao.kt
@@ -1,4 +1,19 @@
 package com.example.chefturnersguidetocooking.database
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface RecipeIngredientDao {
+    @Query("SELECT * FROM RecipeIngredient")
+    fun getAllRecipeIngredients(): Flow<List<RecipeIngredient>>
+
+    @Insert
+    suspend fun insertRecipeIngredient(recIng: RecipeIngredient)
+
+    @Delete
+    suspend fun deleteRecipeIngredient(recIng: RecipeIngredient)
 }
