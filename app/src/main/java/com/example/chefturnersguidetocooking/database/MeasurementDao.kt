67bdb6d89267fe6f1cb6d2f13c32020f9e248776
kruegerj9819 <144365600+kruegerj9819@users.noreply.github.com>
2024-04-07 18:26:03 -0500

    Measurement dao done

--- app/src/main/java/com/example/chefturnersguidetocooking/database/MeasurementDao.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/database/MeasurementDao.kt
@@ -1,4 +1,24 @@
 package com.example.chefturnersguidetocooking.database
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
 interface MeasurementDao {
+    @Query("SELECT * FROM Measurement")
+    fun getAllMeasurements(): Flow<List<Measurement>>
+
+    @Query("SELECT * FROM Measurement WHERE name = :measurementName")
+    fun getMeasurement(measurementName: String): Flow<List<Measurement>>
+
+    @Insert
+    suspend fun insertMeasurement(
+        meas: Measurement
+    )
+
+    @Delete
+    suspend fun deleteMeasurement(
+        meas: Measurement
+    )
 }
