4cf6e3f07a56f3ffdcf3a156f2ad43ccf3debce4
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-04-11 10:56:51 -0500

    Slight changes to View Model

--- app/src/main/java/com/example/chefturnersguidetocooking/database/DatabaseViewModel.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/database/DatabaseViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.chefturnersguidetocooking.database
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class DatabaseViewModel(
     repository: DatabaseRepositoryInterface
@@ -9,7 +11,9 @@ class DatabaseViewModel(
     private val dbRepository: DatabaseRepositoryInterface
 
     private val _dbState = MutableStateFlow(DatabaseState())
+    val dbState: StateFlow<DatabaseState> = _dbState
     init{
         dbRepository = repository
+        Log.d("Database View Model", "Creating Database")
     }
 }
