f3782fe2421a1f1b94d0e7eff1c67c4eed33e567
kruegerj9819 <144365600+kruegerj9819@users.noreply.github.com>
2024-04-11 09:54:07 -0500

    Dummy Code into RecipeDishType to run the code

--- app/src/main/java/com/example/chefturnersguidetocooking/database/RecipeDishType.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/database/RecipeDishType.kt
@@ -1,9 +1,13 @@
 package com.example.chefturnersguidetocooking.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class RecipeDishType(
-
+    @ColumnInfo(name = "rid")
+    val rid: Long,
+    @ColumnInfo(name = "dtid")
+    val dtid: Long
 )
