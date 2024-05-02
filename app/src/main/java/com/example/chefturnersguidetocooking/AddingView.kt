8540a4330ea93514813fbdf4169799cc98c42740
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-05-01 23:11:07 -0500

    Pushing to fix AddingView

--- app/src/main/java/com/example/chefturnersguidetocooking/AddingView.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/AddingView.kt
@@ -336,42 +336,6 @@ fun AddingView(
                         .padding(bottom = 16.dp)
                         .fillMaxWidth()
                 )
-            }
-/*
-
-    var nameInput by remember { mutableStateOf("") }
-    var originInput by remember { mutableStateOf("") }
-    var descriptionInput by remember { mutableStateOf("") }
-    var dishTypeInput by remember { mutableStateOf("") }
-    var prepTimeInput by remember { mutableStateOf("") }
-    var servingsInput by remember { mutableStateOf("") }
-    var cookTimeInput by remember { mutableStateOf("") }
-    var calorieInput by remember { mutableStateOf("") }
-    var carbInput by remember { mutableStateOf("") }
-    var fatInput by remember { mutableStateOf("") }
-    var proteinInput by remember { mutableStateOf("") }
-    val ingredientList  = remember { mutableStateListOf<String>() }
-    val instructionList = remember { mutableStateListOf<Instruction>() }
- */
-            Button(
-                onClick = {
-                    var instructionString = ""
-                    instructionList.forEach{inst ->
-                        instructionString += inst.stepNum.toString() + ". " + inst.instruction + "\n"
-                    }
-                  /*  dbViewModel.insertRecipe(name= nameInput,origin= originInput, favorite= false,
-                        image= null, numCooked= 0, description= descriptionInput,
-                        instructions= instructionString, calories= calorieInput.toInt(),
-                        carbs= carbInput.toInt(),
-                        fat= fatInput.toInt(), protein= proteinInput.toInt(), servings= servingsInput.toInt(),
-                        prepTime= prepTimeInput, cookTime= cookTimeInput,
-                        totalTime = (prepTimeInput.toInt() + cookTimeInput.toInt()).toString())*/
-
-                },
-                modifier = Modifier
-                    .padding(bottom = dimensionResource(R.dimen.padding_medium))
-                    .fillMaxWidth()
-            ) {
                 Text(
                     text = "Nutrition Facts (per serving)",
                     fontWeight = FontWeight.Bold,
@@ -440,10 +404,21 @@ fun AddingView(
                      * The on click function will add all of the data to the database.
                      */
                     onClick = {
-                        /*TODO*/
+                        var instructionString = ""
+                        instructionList.forEach{inst ->
+                            instructionString += inst.stepNum.toString() + ". " + inst.instruction + "\n"
+                        }
+                        /*  dbViewModel.insertRecipe(name= nameInput,origin= originInput, favorite= false,
+                              image= null, numCooked= 0, description= descriptionInput,
+                              instructions= instructionString, calories= calorieInput.toInt(),
+                              carbs= carbInput.toInt(),
+                              fat= fatInput.toInt(), protein= proteinInput.toInt(), servings= servingsInput.toInt(),
+                              prepTime= prepTimeInput, cookTime= cookTimeInput,
+                              totalTime = (prepTimeInput.toInt() + cookTimeInput.toInt()).toString())*/
+
                     },
                     modifier = Modifier
-                        .padding(bottom = 16.dp)
+                        .padding(bottom = dimensionResource(id = R.dimen.padding_crazy_large))
                         .fillMaxWidth()
                 ) {
                     Text(
