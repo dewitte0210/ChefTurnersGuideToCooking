3ad851c21523984f19944cc1c6cd3fccfd7bf479
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2024-05-01 20:21:28 -0500

    Fixed some import conflicts from the Adding-View merge

--- app/src/main/java/com/example/chefturnersguidetocooking/RecipeScreens.kt
+++ app/src/main/java/com/example/chefturnersguidetocooking/RecipeScreens.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -35,7 +34,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -44,9 +42,6 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -59,21 +54,9 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.example.chefturnersguidetocooking.data.ExamplesDataProvider
-import com.example.chefturnersguidetocooking.database.DatabaseViewModel
-import com.example.chefturnersguidetocooking.model.Recipes
-import com.example.chefturnersguidetocooking.ui.theme.RecipeTheme
-import com.example.chefturnersguidetocooking.RecipeContentType
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import com.example.chefturnersguidetocooking.RecipeViewModel
-import androidx.navigation.compose.rememberNavController
 import com.example.chefturnersguidetocooking.database.DatabaseViewModel
 import com.example.chefturnersguidetocooking.database.Recipe
 import com.example.chefturnersguidetocooking.database.SingleRecipeAllInfo
